91344c0f9f6e012b552cbaec8a590ef21087e18c
Rag <enzoboudart@gmail.com>
2020-03-05 20:45:06 +0100

    [ADDED] DataConvertor

---
+++ Assets/NeuralNetwork/Scripts/Systems/SQLReader.cs
@@ -0,0 +1,8 @@
+
+namespace NeuralNetwork
+{
+    public class SQLReader
+    {
+        
+    }
+}
---
+++ DataConvertor/DataConvertor/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.SQLite;
+namespace DataConvertor
+{
+    
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            SQLiteConnection.CreateFile("MyDataBase.dtbase");
+            SQLiteCommand command = new SQLiteCommand();
+            SQLiteDataReader dataReader = command.ExecuteReader();
+            
+            
+
+        }
+    }
+}
